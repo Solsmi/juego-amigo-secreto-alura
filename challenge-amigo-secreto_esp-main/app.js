1e8c85185e429fa7b67f79371ff4feb65e2fe0cb
Solange San Martin Ibañez <solange.sanmartin.i@gmail.com>
2025-08-01 11:21:40 -0400

    Crea arreglo y función para agregar amigos con validación de entrada

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,16 @@
+let amigos = [];
+
+function agregarAmigo() {
+    const nombre = document.getElementById("amigo").value;
+    //  validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+    //  Si el campo no está vacío, agregar el nombre al array amigos y limpiar el campo de entrada.
+    //  Luego, llamar a la función mostrarAmigos() para actualizar la lista de amigos.
+    if (nombre) { // Verificar si el campo no está vacío
+         // Agregar el nombre al array amigos:
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        //mostrarAmigos();
+    } else {
+        alert("Por favor, ingresa un nombre.");
+    }
+}
